ae402eba005a709ddb1cad31f0853e7744de4411
Sateesh9999 <thakkellapatisateesh@gmail.com>
2026-09-03 14:57:59 +0530

    added ipadderss to accept traffic flow

--- BE-Music-Downloader/app.py
+++ BE-Music-Downloader/app.py
@@ -39,7 +39,7 @@ def create_app(config_name=None):
                 "http://localhost:5173",
                 "http://127.0.0.1:5173",
                 "http://192.168.1.45:5173",
-                "http://172.29.128.1:5173/"
+                "http://172.29.128.1:5173"
             ]
         }
     })

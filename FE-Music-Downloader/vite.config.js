52d01b8f4ca1d9cd1df8cc53f5b1ece14eaf63e0
Sateesh9999 <thakkellapatisateesh@gmail.com>
2026-09-13 19:35:33 +0530

    added domain

--- FE-Music-Downloader/vite.config.js
+++ FE-Music-Downloader/vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    allowedHosts: [
+      'tunesforme.shop'
+    ],
     host: '0.0.0.0',
     port: 5173,
     open: false,
